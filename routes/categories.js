299edf734c17ebb1a7a425aa280eff8c8655b44c
Jessica Winters <jskawinters@gmail.com>
2018-11-16 19:50:29 +0000

    Fixed missing closing tag in categories.js

--- routes/categories.js
+++ routes/categories.js
@@ -5,62 +5,64 @@ const router  = express.Router();
 
 module.exports = function(DataHelpers) {
 
-    // Get all categories.
-    router.get("/", (req, res) => {
-        DataHelpers.getCategories((results) => {
-            res.json(results);
-        });
-
-     // After select a Category show this page
-    router.get("/:id", (req, res) => {
-        let categoryId = req.params.id;
-        let categoryData, pointData;
-          DataHelpers.getCategoryByID(categoryId, (results) => {
-            let categoryData = results;
-            DataHelpers.getPoints(categoryId, (results) => {
-              let pointData = results;
-              res.json({category_data: categoryData, point_data: pointData});
-            });
-          });
+  // Get all categories.
+  router.get('/', (req, res) => {
+    DataHelpers.getCategories((results) => {
+      res.json(results);
     });
+  });
 
-    //New Category Page
-    router.get("/new", (req, res) => {
-        res.render("edit-new");
-        //res.json({1:1});
+  // After select a Category show this page
+  router.get('/:id', (req, res) => {
+    let categoryId = req.params.id;
+    let categoryData, pointData;
+
+    DataHelpers.getCategoryByID(categoryId, (results) => {
+      let categoryData = results;
+
+      DataHelpers.getPoints(categoryId, (results) => {
+        let pointData = results;
+
+        res.json({category_data: categoryData, point_data: pointData});
+      });
     });
+  });
+
+  //New Category Page
+  router.get('/new', (req, res) => {
+    res.render('edit-new');
+    //res.json({1:1});
+  });
 
     //Saving New Category
-    router.post("/", (req, res) => {
-        let image = req.body.image;
+    router.post('/', (req, res) => {
+      let image = req.body.image;
 
-        let category =  {
-            name : req.body.title,
-            description: req.body.description,
-            //image : req.body.image
-        }
+      let category =  {
+        name : req.body.title,
+        description: req.body.description,
+        //image : req.body.image
+      };
 
-        DataHelpers.addCategory((category) => {
-            res.json(category);
-        });
+      DataHelpers.addCategory((category) => {
+        res.json(category);
+      });
 
-        res.redirect("/api/categories");
-
-        //console.log("Deu certo");
-
+      res.redirect('/api/categories');
     });
 
     //After select a Category show this page
-    router.get("/:id", (req, res) => {
-        let id = req.params.id;
-        res.json({"id":id});
+    router.get('/:id', (req, res) => {
+      let id = req.params.id;
+      res.json({'id': id});
     });
 
     //Like category
-    router.put("/:id/like", (req, res) => {
+    router.put('/:id/like', (req, res) => {
       if (req.userAuthenticated) {
         let userId = req.session.user_id;
         let categoryId = req.params.id;
+
         DataHelpers.toggleLike(userId, categoryId, (results) => {
           console.log(results);
           res.redirect('/');
@@ -71,9 +73,9 @@ module.exports = function(DataHelpers) {
     });
 
     //update category
-    router.put("/:id/edit", (req, res) => {
-        res.json({"id":id});
+    router.put('/:id/edit', (req, res) => {
+      res.json({'id': id});
     });
 
   return router;
-}
+};
